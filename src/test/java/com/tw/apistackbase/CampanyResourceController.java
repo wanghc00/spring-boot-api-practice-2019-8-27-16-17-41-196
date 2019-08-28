8e9707e95ba55854c63e7e631427bc700b82bb70
wanghc00 <306093226@qq.com>
2019-08-29 00:13:03 +0800

    add first test case --get company info

---
+++ src/test/java/com/tw/apistackbase/CampanyResourceController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C), 1992-2019
+ * Package com.tw.apistackbase 
+ * FileName: CampanyResourceController.java
+ * Author:   wang-hc
+ * Date:     2019年8月29日 上午12:05:59
+ * Description: //模块目的、功能描述      
+ * History: //修改记录
+ *===============================================================================================
+ *   author：          time：                             version：           desc：
+ *   wang-hc           2019年8月29日上午12:05:59                     1.0                  
+ *===============================================================================================
+ */
+package com.tw.apistackbase;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * 名称：〈一句话功能简述〉<br> 
+ * 功能：〈功能详细描述〉<br>
+ * 方法：〈方法简述 - 方法描述〉<br>
+ * 版本：1.0 <br>
+ * 日期：2019年8月29日 <br>
+ * 作者：wang-hc <br>
+ * 说明：<br>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CampanyResourceController {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void shouldReturnStringContains_company001() throws Exception {
+        this.mockMvc.perform(get("/companies")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("company001")));
+    }
+}
